71c579ca1002d9989b7603c8ab1db089599f2025
fcossetta <fcossetta@azinit.it>
2021-12-13 12:16:19 +0100

    add untrusted device handling. Update readme

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 Simple implementation of [SecureDevice](https://github.com/federicocossetta/SecureDevice) lib
 
-This application is using Retrofit as Network provider
+This application is using Retrofit as Network provider, and shows a list of github users 
+connected to device.
+
+It does not shows any data about device, but run only if device is trusted. To access device 
+data collected, use
+
+
+```
+securityChecker.getAnalysisResult()
+```
 
--- app/src/main/java/com/fcossetta/simpletest/MainActivity.kt
+++ app/src/main/java/com/fcossetta/simpletest/MainActivity.kt
@@ -28,42 +28,69 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val builder = SecurityProviderOption.Builder(applicationContext)
         builder.setNetworkClient(SimpleRetrofitClient())
-        securityChecker = SecurityChecker(builder.build())
-        userRepo.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        try {
+            securityChecker = SecurityChecker(builder.build())
+            userRepo.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val repoNameString = repoName
+                        .text.toString()
+                    search_button.isEnabled = s != null && s.toString().isNotEmpty() &&
+                            repoNameString != null && repoNameString.isNotEmpty()
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+            })
+
+            repoName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val userRepoName = userRepo
+                        .text.toString()
+                    search_button.isEnabled = s != null && s.toString().isNotEmpty() &&
+                            userRepoName != null && userRepoName.isNotEmpty()
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+            })
+            search_button.setOnClickListener {
+                searchGitHubUser(userRepo.text.toString(), repoName.text.toString())
             }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val repoNameString = repoName
-                    .text.toString()
-                search_button.isEnabled = s != null && s.toString().isNotEmpty() &&
-                        repoNameString != null && repoNameString.isNotEmpty()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-        })
-
-        repoName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val userRepoName = userRepo
-                    .text.toString()
-                search_button.isEnabled = s != null && s.toString().isNotEmpty() &&
-                        userRepoName != null && userRepoName.isNotEmpty()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-        })
-        search_button.setOnClickListener {
-            searchGitHubUser(userRepo.text.toString(), repoName.text.toString())
+        } catch (c: SecureDeviceException) {
+            showUntrusted()
+            Log.e(TAG, "Device is not secure,can't proceed")
+        } catch (c: Exception) {
+            Log.e(TAG, Log.getStackTraceString(c))
         }
 
+    }
+
+
+    private fun showUntrusted() {
+        untrusted_device.visibility = View.VISIBLE
+        userRepo.visibility = View.GONE
+        repoName.visibility = View.GONE
+        search_button.visibility = View.GONE
+        users_list.visibility = View.GONE
+        empty_view.visibility = View.GONE
     }
 
     private fun searchGitHubUser(userRepo: String, repoName: String) {
@@ -102,14 +129,17 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     override fun onDeviceUntrusted(e: SecureDeviceException) {
+                        Log.d(TAG, "onDeviceUntrusted: " + Log.getStackTraceString(e))
                         Toast.makeText(
                             applicationContext, "Device is not trusted. Can't proceed", Toast
                                 .LENGTH_LONG
                         ).show()
-                        Log.d(TAG, "onDeviceUntrusted: ")
+                        showUntrusted()
                     }
                 })
-            val (isRooted, isEmulated, permissions, deviceName, sdkVersion, packageName, installedApps) = securityChecker.getAnalysisResult()
+
+            val analysisResult = securityChecker.getAnalysisResult()
+            analysisResult.isEmulated
         } catch (c: Exception) {
             Log.d(TAG, "onDeviceUntrusted: " + Log.getStackTraceString(c))
         }
